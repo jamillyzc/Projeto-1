6c8e2aecb857d93b5bfa39c0f7cf4e4c6747ed13
jamillyzc <jamillyzc@gmail.com>
2026-04-28 15:39:14 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title('Teste ECMI 2')
 st.write('Esse é o meu texto')
 
-st.image('https://github.com/jamillyzc/Projeto-1/blob/main/imagem%20de%20gato.jpg')
+st.image('https://raw.githubusercontent.com/jamillyzc/Projeto-1/refs/heads/main/imagem%20de%20gato.jpg')
 
   
