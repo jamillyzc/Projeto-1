20ccf6cfe609c0c9682e90b6aa5ce56cf3481a13
jamillyzc <jamillyzc@gmail.com>
2026-04-28 15:05:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title('Teste ECMI 2')
 st.write('Esse é o meu texto')
+
+print('Seja Bem-Vindo À Plataforma')/n
+nome = input('Qual é o seu nome?')
+idade = int(input('Qual é a sua idade?'))
+email = input('Digite o seu e-mail')
+email1 = input('Confirme o seu e-mail')
+
+if email == e-mail2:
+  
