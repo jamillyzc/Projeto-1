baa8b690626a260f30e3c55e01401df430a6ef88
jamillyzc <jamillyzc@gmail.com>
2026-04-28 15:24:42 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,11 +3,6 @@ import streamlit as st
 st.title('Teste ECMI 2')
 st.write('Esse é o meu texto')
 
-st.print('Seja Bem-Vindo À Plataforma')
-st.nome = input('Qual é o seu nome?')
-st.idade = int(input('Qual é a sua idade?'))
-st.email = input('Digite o seu e-mail')
-st.email1 = input('Confirme o seu e-mail')
-
+st.image('https://github.com/jamillyzc/Projeto-1/blob/main/imagem%20de%20gato.jpg')
 
   
